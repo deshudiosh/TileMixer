17b64192246152e08285c94dccb8d3997aa86ea3
deshu <deshudiosh@gmail.com>
2017-03-20 18:08:01 +0100

    init pull

--- TileMixer.py
+++ TileMixer.py
@@ -1,32 +1,46 @@
 import itertools
-import kivy
-# kivy.require("")
-
 
 from kivy.app import App
-from kivy.uix.image import Image
-from kivy.uix.label import Label
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.widget import Widget
 
-class Root(Widget):
+
+class Root(AnchorLayout):
     pass
 
-class TileGrid(Widget):
+class Tile(Widget):
+    def left(self):
+        print("left")
+
+    def right(self):
+        print("right")
+
+class TileGrid(RelativeLayout):
     def update_grid(self, rows=3, cols=3):
-        for i, j in itertools.product(range(rows), range(cols)):
-            img = Image(source="txt.jpg")
-            img.pos = (i*img.width, j*img.height)
-            self.add_widget(img)
+
+        tileSize = 200, 200
+
+        for i, j in itertools.product(range(cols), range(rows)):
+            tile = Tile()
+            tile.size = tileSize
+            tile.pos = (i*tile.width, j*tile.height)
+            self.add_widget(tile)
+
+        self.size = tileSize[0] * cols, tileSize[1] * rows
+
+        print(self.size)
 
 
 
 class TileMixer(App):
-
     def build(self):
+        root_widget = Root()
+
         tile_grid = TileGrid()
         tile_grid.update_grid()
 
-        root_widget = Root()
         root_widget.add_widget(tile_grid)
 
         return root_widget
